5d1fc706a4dbd92be84b00f9a6230f607041e89f
Ondřej Zeman <zimik@email.cz>
2022-11-24 01:22:26 +0100

    final

--- src/test/java/TestPrimeFinder.java
+++ src/test/java/TestPrimeFinder.java
@@ -4,33 +4,40 @@ import prime_logic.PrimeFinder;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestPrimeFinder {
-     PrimeFinder pf = new PrimeFinder();
-     @Test
-     public void testNegativeNumber(){
-          assertFalse(pf.findPrime(-45));
-     }
-     @Test
-     public void testNumber0(){
-          assertFalse(pf.findPrime(0));
-     }
-     @Test
-     public void testNumber1(){
-          assertFalse(pf.findPrime(1));
-     }
-     @Test
-     public void testNumber2(){
-          assertTrue(pf.findPrime(2));
-     }
-     @Test
-     public void testNumber27(){
-          assertFalse(pf.findPrime(27));
-     }
-     @Test
-     public void testNumber29(){
-          assertTrue(pf.findPrime(29));
-     }
-     @Test
-     public void testNumber27644437(){
-          assertTrue(pf.findPrime(27644437));
-     }
+    PrimeFinder pf = new PrimeFinder();
+
+    @Test
+    public void testNegativeNumber() {
+        assertFalse(pf.findPrime(-23));
+    }
+
+    @Test
+    public void testNumber0() {
+        assertFalse(pf.findPrime(0));
+    }
+
+    @Test
+    public void testNumber1() {
+        assertFalse(pf.findPrime(1));
+    }
+
+    @Test
+    public void testNumber2() {
+        assertTrue(pf.findPrime(2));
+    }
+
+    @Test
+    public void testNumber27() {
+        assertFalse(pf.findPrime(27));
+    }
+
+    @Test
+    public void testNumber29() {
+        assertTrue(pf.findPrime(29));
+    }
+
+    @Test
+    public void testNumber27644437() {
+        assertTrue(pf.findPrime(27644437));
+    }
 }
